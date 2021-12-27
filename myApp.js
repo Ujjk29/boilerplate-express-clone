c7ea3475e8a41fa43fa2d5425098485b554f9f35
Ujjawal Kumar <ujjakuma@amazon.com>
2021-12-27 23:41:09 +0530

    Added chain middleware to create a time server.

--- myApp.js
+++ myApp.js
@@ -24,4 +24,15 @@ app.get(
     }
 );
 
+app.get(
+    "/now",
+    function (req, res, next) {
+        req.time = new Date().toString();
+        next();
+    },
+    function (req, res) {
+        res.send("{time: "+req.time+"}");
+    }
+);
+
 module.exports = app;
